e5c14b12917c83cd980f588beb59380ff1b2c7e2
Gneissy <ahmetserteserr@gmail.com>
2024-04-24 02:54:55 +0300

    Feat: Next Auth with Prisma Adapter for Google OAuth

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,5 @@
 export { createSong } from "./create-song";
 export { deleteSong } from "./delete-song";
 export { getRecommendations } from "./get-recommendations";
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { signIn, signOut } from "@/actions";
+import { auth } from "@/auth";
 import GetRecommendationForm from "@/components/get-recommendation-form";
+import { Button } from "@nextui-org/react";
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth();
+
+    const temp:string = session?.user ? `Hoş geldin ${session.user.name}` : `Kullanıcı yok`;
+
     return (
         <>
             <div> Ana Sayfa </div>
+
+            <form action = {signIn}>
+                <Button type = "submit"> Sign Up </Button>
+            </form>
+            <form action = {signOut}>
+                <Button type = "submit"> Sign Out </Button>
+            </form>
+
+            <div>
+                Auth durumu: { temp }
+            </div>
+
             <GetRecommendationForm />
         </>
     );
